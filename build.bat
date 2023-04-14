f055673e18d33fbeaa585b39226af94283c137de
Jad Meouchy <jad@jadware.com>
2023-04-13 20:50:26 -0700

    rearrange, make it work on windows instead of linux

---
+++ build.bat
@@ -0,0 +1,6 @@
+SET NDK_PROJECT_PATH=%CD%\sqlite3
+ndk-build
+copy /y sqlite3\libs\arm64-v8a\libsqlite3.so Assets\Plugins\Android\arm64-v8a
+copy /y sqlite3\libs\armeabi-v7a\libsqlite3.so Assets\Plugins\Android\armeabi-v7a
+copy /y sqlite3\libs\x86\libsqlite3.so Assets\Plugins\Android\x86
+copy /y sqlite3\libs\x86_64\libsqlite3.so Assets\Plugins\Android\x86_64
